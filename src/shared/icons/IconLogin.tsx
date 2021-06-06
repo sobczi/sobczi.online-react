e8d9009d602cb870ae1e4bc5966da24d2bf6ba34
Daniel Sobczak <danielsobczi@gmail.com>
2021-06-07 01:45:28 +0200

    fix: enable background property

--- src/shared/icons/IconLogin.tsx
+++ src/shared/icons/IconLogin.tsx
@@ -2,7 +2,7 @@ function IconLogin () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      enable-background='new 0 0 24 24'
+      enableBackground='new 0 0 24 24'
       height='100%'
       viewBox='0 0 24 24'
       width='100%'
